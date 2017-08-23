b4d44a203b914ea6b538f34f2c8b7718221bf264
Meghan Spencer <emjayess@gmail.com>
2017-08-23 23:04:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -93,5 +93,9 @@ function removeFromCart(item) {
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber) {
+    console.log(`Your total cost is $${TODO}, which will be charged to the card ${cardNumber}`)
+  } else {
+    console.log(`Sorry, we don't have a credit card on file for you.`);
+  }
 }
